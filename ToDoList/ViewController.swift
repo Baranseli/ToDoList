6ca1bd6a43448c2d83e3ddfa1fcb0013a123c651
Baranseli <baranselias@hotmail.com>
2019-05-02 00:05:57 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Kafkas Baranseli on 02/05/2019.
+//  Copyright © 2019 Baranseli. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
